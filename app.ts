e55631197ba6ee308a666ea722c5ab63e14e9928
Richard Chou <konami99@hotmail.com>
2021-05-22 23:06:09 +1000

    installed typescript

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
