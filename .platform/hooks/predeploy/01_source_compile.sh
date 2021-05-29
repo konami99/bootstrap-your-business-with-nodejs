0950f5fbbec398925a3b643c1bf57edfadbbf3a3
Richard Chou <konami99@hotmail.com>
2021-05-29 12:46:05 +1000

    removed source_compile

--- .platform/hooks/predeploy/01_source_compile.sh
+++ .platform/hooks/predeploy/01_source_compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# export PATH=$PATH:`ls -td /opt/elasticbeanstalk/node-install/node-* | head -1`/bin
+export PATH=$PATH:`ls -td /opt/elasticbeanstalk/node-install/node-* | head -1`/bin
 
-# sudo npm install
-# sudo /var/app/current/node_modules/.bin/tsc -p tsconfig.json
+sudo npm install
+sudo npm run tsc
