686606a18d0766a3943018af77e07b146727b45a
Richard Chou <konami99@hotmail.com>
2021-05-29 12:56:03 +1000

    Update 01_source_compile.sh

--- .platform/hooks/predeploy/01_source_compile.sh
+++ .platform/hooks/predeploy/01_source_compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export PATH=$PATH:`ls -td /opt/elasticbeanstalk/node-install/node-* | head -1`/bin
+# export PATH=$PATH:`ls -td /opt/elasticbeanstalk/node-install/node-* | head -1`/bin
 
 sudo npm install
 sudo npm run tsc
