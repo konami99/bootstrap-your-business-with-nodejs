932cc1480c27cb1589a67fc9e4d13971f755e5d0
Richard Chou <konami99@hotmail.com>
2021-05-30 21:37:58 +1000

    created cluster

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +8,4 @@ app.get('/', (req, res) => {
   res.render('pages/index');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+export default app;
