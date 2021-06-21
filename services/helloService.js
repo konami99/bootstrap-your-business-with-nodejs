7717e7ccd681ceefa741232900429122e497c317
Richard Chou <konami99@hotmail.com>
2021-06-21 22:50:33 +1000

    removed worker

---
+++ services/helloService.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class HelloService {
+    static sayHello() {
+        console.log('hello from HelloService');
+    }
+}
+exports.default = HelloService;
