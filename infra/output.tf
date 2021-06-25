9fc033e8de149be6aae1f740807b467101dfcb49
Richard Chou <konami99@hotmail.com>
2021-06-25 22:34:08 +1000

    Update output.tf

--- infra/output.tf
+++ infra/output.tf
@@ -1,11 +1,11 @@
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.education.address
+  value       = aws_db_instance.main.address
   sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.education.port
+  value       = aws_db_instance.main.port
   sensitive   = true
 }
